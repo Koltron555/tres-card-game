f70700e1ebe790d7f710d388e53bca528d34d776
Koltron555 <tabek007@hsd.k12.or.us>
2023-02-16 09:54:11 -0800

    adding sample content to learn PyQt6

---
+++ tres_card_game/PyQt6widget.py
@@ -0,0 +1,12 @@
+"""
+This will create a GUI that the user can interact with.
+It will be able to display text and images.
+The user can input text into text boxes and the program can use the text.
+"""
+
+from PyQt5.QtGui import*
+from PyQt5.QtWidgets import*
+from PyQt5.QtCore import
+
+
+app = QApplication(sys.argv)
