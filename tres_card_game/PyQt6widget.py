36b3e7fdf79cc3b892a8f8b6e1bd6fa16e7e44e0
Koltron555 <tabek007@hsd.k12.or.us>
2023-03-03 09:35:19 -0800

    Used the widget file to figure out more of how to use the widget Finally changed the interpreter path so now python imports things correctly

--- tres_card_game/PyQt6widget.py
+++ tres_card_game/PyQt6widget.py
@@ -4,9 +4,12 @@ It will be able to display text and images.
 The user can input text into text boxes and the program can use the text.
 """
 
-from PyQt5.QtGui import*
-from PyQt5.QtWidgets import*
-from PyQt5.QtCore import
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
+import sys
 
 app = QApplication(sys.argv)
+
+app.exec_()
